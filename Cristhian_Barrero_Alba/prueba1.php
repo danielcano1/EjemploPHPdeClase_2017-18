af213a2e5aceaa9323e50cbac8fe6f960747d65f
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-19 13:33:20 +0100

    1Actu

--- Cristhian_Barrero_Alba/prueba1.php
+++ Cristhian_Barrero_Alba/prueba1.php
@@ -1,4 +1,18 @@
 <?php
-    echo "PRIMER PHP DE EJEMPLO: Daniel Cano";
+echo "PRIMER PHP DE EJEMPLO: Daniel Cano";
+echo "<hr/>";
+//EJEMPLO DE FUNCIONES
+$resultado = 0;
+function doble($valor1){
+    $resultado = 2 * $valor1;
     
+    return $resultado;
+}
+
+$var1 = 10;
+$resultado1 = doble($var1);
+echo "<br/> la variabe '$resultado' es: " . $resultado;
+echo "<br/> El doble de " . $var1 . " es " . $resultado1;
+echo "<br/> El doble de 15 es: " . doble(15);
+echo "<br/> El dobde del doble de 15 es " . doble(doble(15));
 ?>

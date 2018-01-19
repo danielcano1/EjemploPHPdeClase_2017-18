688a04b157f676651fd315e954ac42e8ffd40f4b
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-19 13:32:31 +0100

    Funciones

--- Mario_Ibanez_Cembellin/prueba1.php
+++ Mario_Ibanez_Cembellin/prueba1.php
@@ -1,4 +1,12 @@
 <?php
-    echo "PRIMER PHP DE EJEMPLO: Daniel Cano";
-    
+function swp (&$x, &$y){
+    $aux=$x;
+    $x=$y;
+    $y = $aux;
+}
+
+$valor1=12;
+$valor2=27;
+swp($valor1, $valor2);
+    echo "<br/>" . $valor1 . " ".$valor2;
 ?>

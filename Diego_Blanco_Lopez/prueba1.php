eb1f99fc3e67bf826dbe631e7f77b06a54b3a601
Diego <Diego@Diegales-PC>
2018-01-19 13:33:30 +0100

    Clase de funciones 19/01/18

--- Diego_Blanco_Lopez/prueba1.php
+++ Diego_Blanco_Lopez/prueba1.php
@@ -1,4 +1,26 @@
 <?php
     echo "PRIMER PHP DE EJEMPLO: Daniel Cano";
     
-?>
+    //EJEMPLO DE FUNCIONES
+    
+    function doble($valor1){
+        $resultado = 2* $valor1;
+        return $resultado;
+    }
+    $var1=10;
+    $resul= doble($var1);
+    echo "<br /> El doble de " . $var1 . " es " . $resul;
+    echo "</br> El doble de 15 es ". doble(15);
+    echo "</br> El doble del doble de 15 es ". doble(doble(15));
+    
+   echo "</br>";
+   function swap(&$x , &$y){ //& hace un cambio literal en la posicion de memoria de la variable
+       $aux=$x;
+       $x=$y;
+       $y=$aux;
+   }
+   $valor1=12;
+   $valor2=17;
+   swap($valor1, $valor2);
+   echo $valor1. " ". $valor2;
+ ?>

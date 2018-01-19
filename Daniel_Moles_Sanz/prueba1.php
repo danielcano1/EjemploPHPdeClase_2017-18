5d08c01641bc915c349f85fbf9c8b861195cf61c
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-19 13:33:22 +0100

    Clase de funciones 19/01/2018

--- Daniel_Moles_Sanz/prueba1.php
+++ Daniel_Moles_Sanz/prueba1.php
@@ -1,4 +1,33 @@
 <?php
-    echo "PRIMER PHP DE EJEMPLO: Daniel Cano";
+    echo "PRIMER PHP DE EJEMPLO: Daniel Moles Sanz";
+    
+    //EJEMPLO DE FUNCIONES
+    $resultado = 0;
+    function doble($valor1){
+        $resultado = 2 * $valor1;
+        
+        return $resultado;
+    }
+    
+    $var1 = 10;
+    doble($var1);
+    echo "<br/> El doble de ". $var1 ." es ". $resultado;
+    echo "<br/> El doble de 15 es:". doble(15);
+    echo "<br/> El doble del doble de 15 es:". doble(doble(15))."<br/>";
+    echo "<hr/>";
+    
+    
+    function swap(&$x,&$y){
+        $aux=$x;
+        $x=$y;
+        $y=$aux;
+    }
+    
+    $valor1 = 12 ;
+    $valor2 = 17 ;
+    swap($valor1, $valor2);
+    echo $valor1." ".$valor2;
+    
+    
     
 ?>

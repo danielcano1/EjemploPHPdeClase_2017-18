586b715f764018014bbd34815cf7e06550033b2e
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-19 13:33:29 +0100

    clase de funciones 19/01/2018

--- Raul_Herranz_Machuca/prueba1.php
+++ Raul_Herranz_Machuca/prueba1.php
@@ -1,4 +1,25 @@
 <?php
-    echo "PRIMER PHP DE EJEMPLO: Daniel Cano";
+    echo "PRIMER PHP DE EJEMPLO: Raul Herranz";
+    //ejemplo de funciones
     
+    function doble($valor1){
+        $resultado = 2 * $valor1;
+        
+        return $resultado;
+    }
+        $var1 = 10;
+        $resul = doble($var1);
+        echo "<br/> El doble de " . $var1 . " es " . $resul;
+        echo "<br/> El doble del doble de 15 es " . doble(doble(15));
+        
+        function swap(&$x, &$y){
+            $aux = $x;
+            $x = $y;
+            $y = $aux;
+        }
+        $valor1 = 12;
+        $valor2 = 17;
+        swap($valor1, $valor2);
+        echo "<br/> " . $valor1 . " " . $valor2;
+        
 ?>

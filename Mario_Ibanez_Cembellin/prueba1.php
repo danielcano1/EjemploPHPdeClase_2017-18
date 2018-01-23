96f3cca9017d87efeb7e89c665df0302a38ed373
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-23 09:34:52 +0100

    Modificacion 1

--- Mario_Ibanez_Cembellin/prueba1.php
+++ Mario_Ibanez_Cembellin/prueba1.php
@@ -1,12 +1,33 @@
 <?php
-function swp (&$x, &$y){
+echo "PRIMER PHP DE EJEMPLO: Daniel Cano";
+echo "<hr/>";
+//EJEMPLO DE FUNCIONES
+$resultado = 0;
+function doble($valor1){
+    $resultado = 2 * $valor1;
+    
+    return $resultado;
+}
+
+$var1 = 10;
+$resultado1 = doble($var1);
+echo "<br/> la variable '$resultado' es: " . $resultado;
+echo "<br/> El doble de " . $var1 . " es " . $resultado1;
+echo "<br/> El doble de 15 es: " . doble(15);
+echo "<br/> El doble del doble de 15 es: " . doble(doble(15));
+
+function swap(&$x, &$y){
     $aux=$x;
-    $x=$y;
+    $x = $y;
     $y = $aux;
 }
 
 $valor1=12;
-$valor2=27;
-swp($valor1, $valor2);
-    echo "<br/>" . $valor1 . " ".$valor2;
+$valor2=17;
+swap($valor1, $valor2);
+echo "<br/> " . $valor1 . " ".$valor2;
+
+
+
+
 ?>

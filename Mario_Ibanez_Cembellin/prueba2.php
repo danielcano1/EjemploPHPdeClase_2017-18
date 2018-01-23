07db940a17c1c5aa68aa1d990c9f79cd813c5e0c
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-23 10:20:43 +0100

    Edicion 23-01-18

---
+++ Mario_Ibanez_Cembellin/prueba2.php
@@ -0,0 +1,30 @@
+
+
+<?php 
+//Parametros predefinidos
+function potencia($base,$exponente=2){
+    $valor=1;
+    for ($i=1;$i<=$exponente;$i++){
+        $valor*=$base;
+    }
+    return $valor;
+}
+
+echo potencia(2,3) . " <br/> "; // no te coge el exponente ya que le das como valor 3
+echo potencia(4); // te coge el exponente que vale 2
+
+
+echo "<hr/>"
+//Variables Gobales
+function encadenar ($numero,$caracterRelleno){
+    global $texto;
+    for($=1;$i<=$numero;$i++){
+        $texto.=$caracterRelleno;    
+    }   
+}
+$texto="Hola";
+encadenar (12,"d");
+echo $texto."<br/>"; //Escribe holaddddddddddd
+encadenar (9,"+-");
+echo $texto. "<br/>";
+?>

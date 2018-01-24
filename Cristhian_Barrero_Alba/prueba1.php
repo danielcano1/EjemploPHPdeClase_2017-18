e9b8485ba27a2eb01a17e3bcd3980fa2b5cb30ff
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-24 13:46:22 +0100

    Actualizacion 23

--- Cristhian_Barrero_Alba/prueba1.php
+++ Cristhian_Barrero_Alba/prueba1.php
@@ -15,4 +15,5 @@ echo "<br/> la variabe '$resultado' es: " . $resultado;
 echo "<br/> El doble de " . $var1 . " es " . $resultado1;
 echo "<br/> El doble de 15 es: " . doble(15);
 echo "<br/> El dobde del doble de 15 es " . doble(doble(15));
+
 ?>

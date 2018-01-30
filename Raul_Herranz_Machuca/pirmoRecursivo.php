0cd805e4ba292e62f946d4cb8ee4071bd6db3a1b
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-01-30 10:20:59 +0100

    30/01/2018

--- Raul_Herranz_Machuca/pirmoRecursivo.php
+++ Raul_Herranz_Machuca/pirmoRecursivo.php
@@ -1,8 +1,18 @@
 <?php
 function esPrimo($primo, $divisor=2){
-    if ($primo = $divisor) return true;
-    elseif ($primo % $divisor == 0) return false;
-    else esPrimo($primo, $divisor+1);
+    if ($primo % $divisor == 0){
+        return false;
+    }
+    if ($primo = $divisor){
+        return true;
+    }
+    else {
+        return esPrimo($primo, $divisor+1);
+    }
 }
-if (esPrimo(8)) echo "es primo";
-else echo "no es primo";
+if (esPrimo(6)){
+    echo "es primo";
+}
+else{
+    echo "no es primo";
+}
--- Raul_Herranz_Machuca/prueba1.php
+++ Raul_Herranz_Machuca/prueba1.php
@@ -64,4 +64,16 @@
             else return $n*factorial($n-1);
         }
         echo factorial(5);
+        //ARRAYS
+        $numero=array(17, 34, 3=>45, 2, 7=>9, 10=>-5, 7);
+        $desplazamiento=0;
+        
+        for ($i=0; $i<(count($numero)+$desplazamiento);$i++){
+            if (isset($numero[$i])){
+                echo $numero[$i] . "<br/>";
+            }
+            else {
+                $desplazamiento++;
+            }
+        }
 ?>

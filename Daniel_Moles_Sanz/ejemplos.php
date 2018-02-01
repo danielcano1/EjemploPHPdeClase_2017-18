0d9fd7d0cb5e31640fb14a2de8c4dd97fb9fa9d4
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-02-01 08:34:00 +0100

    Daniel - Arrays Multidimensionales 31-1

--- Daniel_Moles_Sanz/ejemplos.php
+++ Daniel_Moles_Sanz/ejemplos.php
@@ -65,4 +65,54 @@ for ($i=0;$i<(count($numero)+$desplazamiento);$i++){
         $desplazamiento++;
     }
 }
+
+echo "<hr/>";
+
+$nota=array("Antonio"=>5, 4, 7, 5=>6, "Luis"=>9, 2, "Berta"=>7);
+foreach ($nota as $i=>$v){
+    echo "La nota de $i es $v"."<br/>";
+}
+
+echo "<hr/>";
+
+//$nota2[0]=9;
+//$nota2[1]=7;
+//$nota[0]=$nota2;
+
+//echo $nota[0][0]."<br/>"; 
+//echo $nota[0][1];
+
+
+$notaA[0][0][0]="Hola";
+$notaA[0][0][1]="Adios";
+$notaA[0][1][0]="Pepe";
+$notaA[0][1][1]="Juan";
+$notaA[1][0][0]="Fin";
+$notaA[1][0][1]="Inicio";
+$notaA[1][1][0]="[";
+$notaA[1][1][1]="]";
+echo $notaA[1][1][0]." ".$notaA[0][0][0]." ".$notaA[0][1][0]." ".$notaA[1][1][1]."<br/>";
+echo $notaA[1][1][0]." ".$notaA[0][0][1]." ".$notaA[0][1][0]." ".$notaA[1][1][1]."<br/>";
+echo $notaA[0][1][1]." ".$notaA[0][0][1]." ".$notaA[1][1][0].$notaA[0][1][0].$notaA[1][1][1]."<br/>";
+echo "<hr/>";
+$saludos[0]="Hola";
+$saludos[1]="Adios";
+$nombres[0]="Pepe";
+$nombres[1]="Juan";
+$estados[0]="Fin";
+$estados[1]="Inicio";
+$extremos[0]="[";
+$extremos[1]="]";
+
+
+$conversacion[0]=$saludos;
+$conversacion[1]=$nombres;
+$reglas[0]=$estados;
+$reglas[1]=$extremos;
+$todos[0]=$conversacion;
+$todos[1]=$reglas;
+echo $todos[1][1][0]." ".$todos[0][0][0]." ".$todos[0][1][0]." ".$todos[1][1][1]."<br/>";
+echo $todos[1][1][0]." ".$todos[0][0][1]." ".$todos[0][1][0]." ".$todos[1][1][1]."<br/>";
+echo $todos[0][1][1]." ".$todos[0][0][1]." ".$todos[1][1][0].$todos[0][1][0].$todos[1][1][1]."<br/>";
+
 ?>

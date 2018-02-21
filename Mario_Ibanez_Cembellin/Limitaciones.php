061f2b3a20d149c4770bc73cb35b33259d732e9f
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-02-21 18:37:23 +0100

    Limitaciones

---
+++ Mario_Ibanez_Cembellin/Limitaciones.php
@@ -0,0 +1,37 @@
+<?php
+//Variables globales
+$nombreproyecto = "Limitaciones.php";
+$intentos=0;
+
+//HTML FORMULARIO ACCESO
+
+
+
+if(isset($_GET["usuario"]) && isset($_GET["pass"])){
+    if($_GET["usuario"] == "valido" && $_GET["pass"] == "hola") {
+        echo "Hola bienvenido";
+    }
+    else{
+        echo "Error";
+           $intetos=$intentos+1;
+    }
+}
+
+           
+
+
+// funcion cookie es menor que tres
+function numeroDeIntentos(){
+    global $intentos;
+    setcookie("VecesAcceso", $intentos); 
+    
+}
+    
+    
+  
+    
+echo "<form action='" . $nombreproyecto . "' method='get'>
+        Usuario: <input type='text' name='usuario'/>
+        Contraseña: <input type='text' name='pass'/>
+                <input type='submit' value='Enviar' name='enviar'/>
+      </form>";

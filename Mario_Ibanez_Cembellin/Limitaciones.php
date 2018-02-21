ed1d153e7ccf648c0c134e5cecf6d218db95a4e3
Usuario <Usuario@10.0.2.15>
2018-02-21 19:53:08 +0100

    Limitaciones Mario

--- Mario_Ibanez_Cembellin/Limitaciones.php
+++ Mario_Ibanez_Cembellin/Limitaciones.php
@@ -3,50 +3,44 @@
 $nombreproyecto = "Limitaciones.php";
 $intentos=0;
 
+//Intento
 
+if(isset($_GET["usuario"]) && isset($_GET["pass"]))
+{
+    
 
-if(isset($_GET["usuario"]) && isset($_GET["pass"])){
-<<<<<<< HEAD
     
-    
-    
-    if($_GET["usuario"] == "valido" && $_GET["pass"] == "hola") {
-        echo "Hola bienvenido";
-=======
-    if($_COOKIE["VecesAcceso"]<=3){
-   
-        if($_GET["usuario"] == "valido" && $_GET["pass"] == "hola") {
-            echo "Hola bienvenido";
+    if($_COOKIE["intentos"] < 3 && $_COOKIE["intentos"] > 0)
+    {
+        if($_GET["usuario"] == "mario" && $_GET["pass"] == 1234) 
+        {
+        
+            echo "Hola has entrado";
         }
-        else{
-            echo "Error";
-               $intetos=$intentos+1;
-               numeroDeIntentos();
-        }   
->>>>>>> branch 'master' of https://github.com/danielcano1/EjemploPHPdeClase_2017-18
+        
+        else {
+            $intentos=$intentos+1;
+            echo "Error ";
+            grabarIntentoCookie();
+        }
+         
+        }
+        else {
+            echo "Sin intentos prueba en un rato ";
+            setcookie("intentos",4,time()+120);
+            
+        }
     }
-<<<<<<< HEAD
-    else{
-        echo "Error";
-           $intetos=$intentos+1;
-    }
-    numeroDeIntentos();
-=======
-    else 
-            echo "Numero de intentos Maximos";
-                setcookie("VecesAcceso",4,time()+1);
->>>>>>> branch 'master' of https://github.com/danielcano1/EjemploPHPdeClase_2017-18
-}
+     
+    
 
-           
-
+          
 
 // funcion cookie es menor que tres
-function numeroDeIntentos(){
-    global $intentos;
-    setcookie("VecesAcceso", $intentos); 
-    
-}
+    function grabarIntentoCookie(){
+        global $sesionIntentada;
+        setcookie("grabar",$sesionIntentada);
+    }
     
     
 //HTML FORMULARIO ACCESO

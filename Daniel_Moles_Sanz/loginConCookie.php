46416b64bdc99ec65c1793157ca57a4647fac7c4
Usuario <Usuario@DESKTOP-0VHOMTJ>
2018-03-01 10:33:58 +0100

    Login con intentos Daniel

--- Daniel_Moles_Sanz/loginConCookie.php
+++ Daniel_Moles_Sanz/loginConCookie.php
@@ -1,30 +1,38 @@
 <?php
 //Variables GLobales
 $numeroDeIntentos=0;
-setcookie("intento",$numeroDeIntentos);
+if (isset($_COOKIE["intento"])){
+    $cookie=$_COOKIE["intento"];
+} else {
+    setcookie("intento",$numeroDeIntentos,time()+120);
+    $numeroDeIntentos++;
+    cargarFormulario();
+}
 
 if (isset($_POST["usuario"]) && isset($_POST["password"])){
-    echo $_COOKIE["intento"];
-    if ($_COOKIE["intento"] <= 3){
+    if ($cookie < 3){
         if ($_POST["usuario"] == "daniel" && $_POST["password"] == "moles"){
             echo "Correcto";
         } else {
-            echo "Error";
+            $numeroDeIntentos=$_COOKIE["intento"];
+            echo "Error, vuelve a intentarlo. Le quedan ".$numeroDeIntentos;           
             $numeroDeIntentos++;
-            setcookie("intento",$numeroDeIntentos);
-            
+            setcookie("intento",$numeroDeIntentos,time()+120);
+            cargarFormulario();
         }
     } else {
-        echo "Demasiados Intentos";
+        echo "Prueba en un rato";
+        setcookie("intento",$numeroDeIntentos,time()+120);
     }
-    
-    
-
 }
 
+
+
 //Formulario Login
+function cargarFormulario(){
 echo "<form action='loginConCookie.php' method='post'>
         Equipo: <input type='text' name='usuario'/>
         Escudo: <input type='text' name='password'/>
                 <input type='submit' value='Enviar'/>
       </form>";
+}

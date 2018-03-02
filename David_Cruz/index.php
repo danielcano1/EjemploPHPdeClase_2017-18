80f7b81b1ae270b127067cb438db4f00f2de3d52
alumno <alumno@DESKTOP-FB4IIJD>
2018-03-02 19:49:45 +0100

    david cruz 02/03

---
+++ David_Cruz/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<form action='comunio.php' method='post'>
+        <label for='nombreUsuario'>Escriba el nombre del usuario</label>
+        <input type='text' name='nombreUsuario'/>
+        <label for='contraseña'>Escriba la contraseña</label>
+        <input type='password' name='contraseña'/>
+        <input type='submit' value='enviar'/>
+       </form><br/>";
